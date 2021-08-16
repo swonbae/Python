c7afaf698473bbcd11455c9ac7246e38bae7817a
swonbae <sbae13@my.centennialcollege.ca>
2021-08-15 22:05:55 -0400

    Added Ch15 - Combining Select Pages from Many PDFs

---
+++ automatetheboringstuff/ch15-Documents/combinePdfs.py
@@ -0,0 +1,28 @@
+#! python3
+# combinePdfs.py - Combines all the PDFs in the current working directory into a single PDF.
+# from 'https://automatetheboringstuff.com/'
+# Updated by SeungWon Bae
+
+import PyPDF2, os
+
+# Get all the PDF filenames
+pdfFiles = []
+for filename in os.listdir('.'):
+    if filename.endswith('.pdf'):
+        pdfFiles.append(filename)
+pdfFiles.sort(key = str.lower)
+pdfWriter = PyPDF2.PdfFileWriter()
+
+# Loop through all the PDF files
+for filename in pdfFiles:
+    pdfFileObj = open(filename, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+    # Loop through all the pages (except the first) and add them
+    for pageNum in range(1, pdfReader.numPages):
+        pageObj = pdfReader.getPage(pageNum)
+        pdfWriter.addPage(pageObj)
+
+# Save the resulting PDF to a file
+with open('allminutes.pdf', 'wb') as pdfOutput:
+    pdfWriter.write(pdfOutput)
